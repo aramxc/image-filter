0e6f33e97e91ca0c6ca37e8321d18b8e90393d15
Aaron Ramirez <aramxc@gmail.com>
2017-09-18 15:27:06 -0600

    separated functions into smaller pieces for increased flexibility

--- Resources/Javascript/index.js
+++ Resources/Javascript/index.js
@@ -8,14 +8,24 @@ function upload() {
   //innstantiate new SimpleImage using fileinput:
   image = new SimpleImage(fileinput);
 
-  //Get image dimensions and return to output:
-  var xDim = image.width;
-  var yDim =  image.height;
-  document.getElementById('dim-output').innerHTML = xDim + ' x ' + yDim;
+  //pass newly created image into other functions:
+  window.onload(draw(image));
+  window.onload(getDimensions(image));
+}
 
+function draw() {
   //Draw image to canvas:
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
   image.drawTo(canvas);
+
+}
+
+//Get image dimensions and return to output:
+function getDimensions() {
+  //TODO: FIX THIS FUNCTION
+  var xDim = 10;
+  var yDim = 10;
+  document.getElementById('dim-output').innerHTML = xDim + ' x ' + yDim;
 
 }
